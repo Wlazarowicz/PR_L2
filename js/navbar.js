4a11c37a01da90d3170a8f1a89732525a39635ab
Wiktor Lazarowicz <wiktorlazarowicz5@gmail.com>
2021-03-19 11:14:08 +0100

    navbar

---
+++ js/navbar.js
@@ -0,0 +1,5 @@
+let nav = '<nav><li><a href="index.html">Strona Główna</a></li>' + '<li><a href="contact.html">Kontakt</a></li></nav>';
+
+let navbarElem = document.querySelector(".navbar");
+
+navbarElem.innerHTML = nav;
